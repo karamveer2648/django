062af0125a248a8dd1a69872c3829a62f510890e
karamfab <karamoberoi19@gmail.com>
2024-12-31 23:19:05 +0530

    zz
    zzz

--- core/home/views.py
+++ core/home/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+    from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+    # Create your views here.
+    from django.http import HttpResponse
 
-def home(request):
-    return render(request,"index.html")
-def about(request):
-    return render(request,"about.html")
-def contact(request):
-    return render(request,"contact.html")
-def test(request):
-    return render(request,"test.html")
+    def home(request):
+        return render(request,"index.html")
+    def about(request):
+        return render(request,"about.html")
+    def contact(request):
+        return render(request,"contact.html")
+    def test(request):
+        return render(request,"test.html")
